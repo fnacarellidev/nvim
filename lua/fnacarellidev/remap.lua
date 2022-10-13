9512471392afb964f95f4ec74af995e7275c221e
fnacarellidev <nacarellifabio@hotmail.com>
2022-10-13 17:58:12 +0200

    change NERDTree Keybinds

--- lua/fnacarellidev/remap.lua
+++ lua/fnacarellidev/remap.lua
@@ -3,8 +3,8 @@ local nnoremap = require("fnacarellidev.keymap").nnoremap
 nnoremap("<leader>pv", "<cmd> Ex <CR>")
 
 nnoremap("<leader>s", ":so %<CR>") -- source current file
-nnoremap("<leader>q", "<cmd> NERDTreeToggle <CR>")
-nnoremap("<leader>n", "<cmd> NERDTreeFocus <CR>")
+nnoremap("<leader> ", "<cmd> NERDTreeToggle <CR>")
+nnoremap("<leader>t", "<cmd> NERDTreeFocus <CR>")
 
 nnoremap("<C-j>", ":m +1 <CR>") -- CTRL + J Move line down
 nnoremap("<C-k>", ":m .-2 <CR>") -- CTRL + K Move line up
