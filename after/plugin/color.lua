69da3fb00f2824832750e760846c7959c57e7173
Fabio Nacarelli <nacarellifabio@hotmail.com>
2023-01-31 12:28:21 -0300

    switched themes

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,15 +1,3 @@
--- vim.g.tokyonight_transparent_sidebar = true
--- vim.g.tokyonight_transparent_background = true
--- vim.opt.background = "dark"
-
--- vim.cmd("colorscheme tokyonight")
--- vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None" })
-end
-
-ColorMyPencils("rose-pine")
+vim.g.edge_dim_foreground = 1
+vim.g.edge_transparent_background = 2
+vim.cmd.colorscheme("edge")
