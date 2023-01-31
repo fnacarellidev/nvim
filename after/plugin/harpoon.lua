5a79448f59f02c86f94249f9bdf5b22b389a7fdb
Fabio Nacarelli <nacarellifabio@hotmail.com>
2023-01-31 14:38:22 -0300

    fixed keybindins for harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,7 +5,7 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>a", mark.add_file)
 keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
-keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
-keymap.set("n", "<C-3>", function() ui.nav_file(3) end)
-keymap.set("n", "<C-4>", function() ui.nav_file(4) end)
+keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
+keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
+keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
+keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
