a73fc77e641ab055d1bdc40ceaec1a0332105650
Fabio Nacarelli <nacarellifabio@hotmail.com>
2023-01-31 12:27:57 -0300

    semantics, leader d quits current buffer

--- lua/fnacarellidev/remap.lua
+++ lua/fnacarellidev/remap.lua
@@ -1,19 +1,23 @@
-vim.keymap.set("n", "<C-j>", "<cmd>m +1 <CR>") -- CTRL + J Move line down
-vim.keymap.set("n", "<C-k>", "<cmd>m.-2 <CR>") -- CTRL + K Move line up
+local keymap = vim.keymap
 
-vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
+keymap.set("n", "<C-j>", "<cmd>m +1 <CR>") -- CTRL + J Move line down
+keymap.set("n", "<C-k>", "<cmd>m.-2 <CR>") -- CTRL + K Move line up
 
-vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<CR>")
-vim.keymap.set("n", "<leader>n", "<cmd>NvimTreeFocus<CR>")
+keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
+keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<leader>l", "<cmd>BufferNext<CR>")
-vim.keymap.set("n", "<leader>h", "<cmd>BufferPrevious<CR>")
-vim.keymap.set("n", "<leader>e", "<cmd>BufferClose<CR>")
+keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<CR>")
+keymap.set("n", "<leader>n", "<cmd>NvimTreeFocus<CR>")
 
-vim.keymap.set("n", "<leader>q", "<cmd> wq <CR>")
-vim.keymap.set("n", "<leader>w", "<cmd> w <CR>")
+keymap.set("n", "<leader>l", "<cmd>BufferNext<CR>")
+keymap.set("n", "<leader>h", "<cmd>BufferPrevious<CR>")
+keymap.set("n", "<leader>e", "<cmd>BufferClose<CR>")
 
--- netrw
-vim.keymap.set("n", "<leader>pv", "<cmd>Ex <CR>")
-vim.keymap.set("n", "<leader>o", "<cmd>Rex <CR>")
+keymap.set("n", "<leader>q", "<cmd> wq <CR>")
+keymap.set("n", "<leader>w", "<cmd> w <CR>")
+
+keymap.set("n", "<leader>d", "<cmd> bd <CR>")
+
+--netrw
+keymap.set("n", "<leader>pv", "<cmd>Ex <CR>")
+keymap.set("n", "<leader>o", "<cmd>Rex <CR>")
