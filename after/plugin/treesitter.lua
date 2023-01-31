696bd7a4f81ad66104cc2990651138bf2a14a88b
Fabio Nacarelli <nacarellifabio@hotmail.com>
2023-01-31 12:29:58 -0300

    removed a bunch of comments and enabled incremental selection

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,20 +1,13 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "bash", "c", "lua", "rust", "cpp" },
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    disable = { "c", "rust" },
-    additional_vim_regex_highlighting = false,
-  },
+	ensure_installed = {
+		"bash", "c", "lua", "rust", "cpp"
+	},
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	incremental_selection = {
+		enable = true,
+	}
 }
