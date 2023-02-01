252f34bd9cd855cf725b067dac0b58c735d49f0b
Fabio Nacarelli <nacarellifabio@hotmail.com>
2023-01-31 23:22:43 -0300

    lsp related stuff

---
+++ after/plugin/lsp/lspsaga.lua
@@ -0,0 +1,27 @@
+local saga_status, saga = pcall(require, "lspsaga")
+if not saga_status then
+	return
+end
+
+saga.setup({
+	-- keybinds for navigation in lspsaga window
+	scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
+	-- use enter to open file with definition preview
+	definition = {
+		edit = "<CR>",
+	},
+	ui = {
+		colors = {
+			normal_bg = "#022746",
+		},
+		theme = "round"
+	},
+	symbol_in_winbar = {
+		enable = true,
+		separator = " ➜ ",
+		show_file = true,
+		respect_root = true,
+		folder_level = 1,
+		color_mode = true
+	}
+})
