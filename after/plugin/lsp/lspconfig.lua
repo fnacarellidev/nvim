cc712dae07e16ec5c3b0997d014f2cd1106c67c4
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-03 19:12:11 -0300

    added diagnostics symbols

--- after/plugin/lsp/lspconfig.lua
+++ after/plugin/lsp/lspconfig.lua
@@ -29,6 +29,11 @@ end
 
 -- used to enable autocompletion
 local capabilities = cmp_nvim_lsp.default_capabilities()
+local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
 
 lspconfig["clangd"].setup({
 	capabilities = capabilities,
