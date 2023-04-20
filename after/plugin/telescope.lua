a984c6cf6eccd4eac0849ea58850017a474df85e
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-20 13:31:48 -0300

    changed some keybinds and not loading fzf because couldn't install it in 42, need to figure it out later

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,13 +1,17 @@
-local builtin = require('telescope.builtin')
 local keymap = vim.keymap
-
 local telescope_setup, telescope = pcall(require, "telescope")
+local builtin_setup, builtin = pcall(require, 'telescope.builtin')
+local actions_setup, actions = pcall(require, "telescope.actions")
+
 if not telescope_setup then
 	return
 end
 
-local actions_setup, actions = pcall(require, "telescope.actions")
 if not actions_setup then
+	return
+end
+
+if not builtin_setup then
 	return
 end
 
@@ -18,15 +22,15 @@ telescope.setup({
 			i = {
 				["<C-k>"] = actions.move_selection_previous,
 				["<C-j>"] = actions.move_selection_next,
-				["<C-g>"] = actions.move_to_top,
-				["<C-b>"] = actions.move_to_bottom,
+				["<C-p>"] = actions.preview_scrolling_up,
+				["<C-n>"] = actions.preview_scrolling_down,
 				["<esc>"] = actions.close
 			}
 		}
 	}
 })
 
-telescope.load_extension("fzf")
+-- telescope.load_extension('fzf')
 keymap.set('n', '<leader>ff', builtin.find_files, {})
 keymap.set('n', '<leader>fs', builtin.live_grep, {})
 keymap.set('n', '<leader>fc', builtin.grep_string, {})
