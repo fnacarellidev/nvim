3f1a7c10fc98b7965a2c6945693ab4cddf38fdb7
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-20 13:29:58 -0300

    catpuccin theme

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,3 +1,5 @@
-vim.g.edge_dim_foreground = 1
-vim.g.edge_transparent_background = 2
-vim.cmd.colorscheme("edge")
+require("catppuccin").setup({
+	flavour = "frappe",
+	transparent_background = true,
+})
+vim.cmd.colorscheme("catppuccin")
