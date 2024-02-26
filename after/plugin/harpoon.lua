d43f7183de1b828ad1ee4464c51b24f84a19f581
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-02-26 17:24:20 -0300

    feat: basic harpoon setup

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,11 +1,9 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-local keymap = vim.keymap
+local harpoon = require('harpoon')
+harpoon:setup()
 
-keymap.set("n", "<leader>a", mark.add_file)
-keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-l>", function()
+	harpoon:list():clear()
+end)
