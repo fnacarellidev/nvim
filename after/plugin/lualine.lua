29bf502199dfc8c294c0f0df6d52876d0d2fe5ec
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-02-26 17:24:47 -0300

    feat: using catppuccin theme on lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,19 +1,5 @@
-local edge_customized = require("lualine.themes.edge")
-
-local my_colors = {
-	blue = "#00AAFF",
-	cyan = "#00FFD4",
-	violet = "#FF61EF",
-	yellow = "#FFF700",
-}
-
-edge_customized.normal.a.bg = my_colors.blue
-edge_customized.insert.a.bg = my_colors.cyan
-edge_customized.visual.a.bg = my_colors.violet
-edge_customized.command.a.bg = my_colors.yellow
-
 require('lualine').setup({
 	options = {
-		theme = edge_customized,
+		theme = "catppuccin",
 	},
 })
